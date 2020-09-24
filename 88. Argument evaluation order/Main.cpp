81a48392bac60c95fc7a93878c28a1d798a3276b
Lead0b110010100 <Lead0b110010100@outlook.com>
2020-09-24 17:04:03 +0200

    Lections 88-89.

---
+++ 88. Argument evaluation order/Main.cpp
@@ -0,0 +1,16 @@
+﻿#include "../Utils/StdAfx.h"
+
+int32_t a = 2;
+
+void func(int32_t a, int32_t b, int32_t c)
+{
+	LOG(a << " " << b << " " << c);
+}
+
+int main()
+{
+	// Undefined behaviour unless you use -std=c++17 flag
+	func(a--, a++, a++);
+	LOG(a);
+	return EXIT_SUCCESS;
+}
---
+++ 89. Move semantics/Main.cpp
@@ -0,0 +1,45 @@
+﻿#include "../Utils/StdAfx.h"
+
+class String
+{
+public:
+	String() = default;
+
+	String(const char* string)
+	{
+		LOG("Created");
+	}
+
+	String(const String& other)
+	{
+		LOG("Copied!");
+	}
+
+	String(String&& moved) noexcept
+	{
+		LOG("Moved!");
+	}
+
+	~String()
+	{
+		LOG("Destroyed!");
+	}
+};
+
+class Entity
+{
+public:
+	Entity(String&& str) : m_data(std::move(str))
+	{
+
+	}
+private:
+	String m_data;
+};
+
+int main()
+{
+	Entity e("Ertugrul");
+
+	return EXIT_SUCCESS;
+}
