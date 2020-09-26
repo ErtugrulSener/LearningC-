931980c0375ccc9b8265aad624f366cceaf705a9
Lead0b110010100 <Lead0b110010100@outlook.com>
2020-09-26 10:22:29 +0200

    Lection 90.

---
+++ 90. Move Assignment Operator and STL move/Main.cpp
@@ -0,0 +1,57 @@
+﻿#include "../Utils/StdAfx.h"
+
+class String
+{
+public:
+	String() = default;
+
+	String(const char* string)
+	{
+		LOG("Created");
+	}
+
+	String(const String& other)
+	{
+		LOG("Copied!");
+	}
+
+	String(String&& moved) noexcept
+	{
+		LOG("Moved!");
+	}
+
+	String& operator=(String&& other) noexcept
+	{
+		if (this != &other)
+		{
+			LOG("Moved with assignment")
+			return other;
+		}
+	}
+
+	~String()
+	{
+		LOG("Destroyed!");
+	}
+};
+
+class Entity
+{
+public:
+	Entity(String&& str) : m_data(std::move(str))
+	{
+
+	}
+private:
+	String m_data;
+};
+
+int main()
+{
+	String apple = "Apple";
+	String dest;
+
+	dest = std::move(apple);
+
+	return EXIT_SUCCESS;
+}
