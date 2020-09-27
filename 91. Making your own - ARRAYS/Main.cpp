1d02e9ff743f304f8b19a1c166e79803d788109d
Lead0b110010100 <Lead0b110010100@outlook.com>
2020-09-27 12:36:50 +0200

    Lection 91.

---
+++ 91. Making your own - ARRAYS/Main.cpp
@@ -0,0 +1,46 @@
+﻿#include "../Utils/StdAfx.h"
+
+template<typename T, size_t S>
+class Array
+{
+public:
+	Array()
+	{
+		LOG("Created an Array");
+	}
+
+	~Array()
+	{
+		LOG("Destroyed an Array");
+	}
+
+	void PrintElements()
+	{
+		for (const auto& d: m_Data)
+		{
+			LOG(d);
+		}
+	}
+
+	T& operator[](size_t index) { return m_Data[index]; }
+	const T& operator[](size_t index) const { return m_Data[index]; }
+
+	T* Data() { return m_Data; };
+	const T* Data() const { return m_Data; };
+
+	constexpr size_t Size() const { return S; }
+
+private:
+	T m_Data[S];
+};
+
+int main()
+{
+	Array<int32_t, 5> data;
+	memset(&data[0], 0, sizeof(data));
+
+	data[1] = 4;
+
+	data.PrintElements();
+	return EXIT_SUCCESS;
+}
